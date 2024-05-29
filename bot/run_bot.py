cbea56b63b2297f0e8abf430be669b6f871b4fb5
Takai <tkbunbury@gmail.com>
2024-05-29 13:30:15 +0100

    deleted code

--- bot/run_bot.py
+++
@@ -1,8 +0,0 @@
-from bot.bot import Bot
-from infrastructure.instrument_collection import instrumentCollection
-
-
-if __name__ == "__main__":
-    instrumentCollection.LoadInstrumentsDB()
-    b = Bot()
-    b.run()
